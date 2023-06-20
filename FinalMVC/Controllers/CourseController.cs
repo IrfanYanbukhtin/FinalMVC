868674b159d29913d7c353d934b15a6ea54f4d47
irfan <irfanfy@code.edu.az.com>
2023-06-20 18:48:15 +0400

    Add project files.

---
+++ FinalMVC/Controllers/CourseController.cs
@@ -0,0 +1,48 @@
+﻿using FinalMVC.DAL;
+using FinalMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalMVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        public readonly AppDbContext _dbContext;
+        private readonly int _courseCount;
+
+        public CourseController(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+            _courseCount = _dbContext.Courses.Count();
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.CourseCount = _courseCount;
+
+            var courses = _dbContext.Courses.Take(3).ToList();
+
+            var viewModel = new CourseViewModel
+            {
+                Courses = courses,
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var course = _dbContext.Courses.Find(id);
+
+            return View(course);
+        }
+
+        public IActionResult LoadCourses(int skipCourse)
+        {
+            var courses = _dbContext.Courses.Skip(skipCourse).Take(3).ToList();
+
+            return PartialView("_CoursePartial", courses);
+        }
+    }
+}
---
+++ FinalMVC/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using FinalMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using FinalMVC.DAL;
+using FinalMVC.DAL.Entities;
+
+namespace FinalMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        public HomeController(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var sliderImage = await _dbContext.SliderImages.ToListAsync();
+            var notice = await _dbContext.Notices.ToListAsync();
+            var noticeInfo = await _dbContext.NoticeInfos.ToListAsync();
+            var banner = await _dbContext.Banners.ToListAsync();
+            var course = await _dbContext.Courses.ToListAsync();
+            var events = await _dbContext.Events.ToListAsync();
+            var testimonial = await _dbContext.Testimonials.ToListAsync();
+            var blog = await _dbContext.Blogs.ToListAsync();
+
+            var viewModel = new HomeViewModel
+            {
+                SliderImages = sliderImage,       
+                Notices = notice,
+                NoticesInfo = noticeInfo,
+                Banners = banner,
+                Courses = course,
+                Events = events,
+                Testimonials = testimonial,
+                Blogs = blog,
+
+            };
+
+            return View(viewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
